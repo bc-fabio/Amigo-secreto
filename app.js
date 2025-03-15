e72e9296485ea0dbf56507eee7742d6422745169
bc-fabio <devfabiolima@gmail.com>
2025-03-15 12:38:11 -0400

    adicionando amigo

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+function adicionarAmigo{
+    let inserirNome = document.querySelector('input').value;
+    if (inserirNome ==""){
+        alert("Por favor, insira um nome");
+    }else{
+         amigos.push();
+    }
+}
