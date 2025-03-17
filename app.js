e9d9e6539f1d823d8fa9eb6f4bd636bda3c9130f
bc-fabio <devfabiolima@gmail.com>
2025-03-16 21:18:30 -0400

    resultado final

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 let amigos = [];
-let maisAmigos = ;
-function adicionarAmigo{
-    let inserirNome = document.getElementById('amigo').value;
-    if (inserirNome ==""){
+function adicionarAmigo(){
+    let inserir = document.getElementById('amigo').value;
+    if (inserir ==""){
         alert("Por favor, insira um nome");
-    }else{
-         maisAmigos.push(amigos);
     }
+    amigos.push(inserir);
+    atualizarNomes();
+    limparCampo();
 }
-function obterNomes{
+function limparCampo() {
+    inserir = document.querySelector('input');
+    inserir.value = '';
+}
+function atualizarNomes(){
     let lista = document.getElementById('listaAmigos');
     lista.innerHTML = "";
+    amigos.forEach(amigo => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
 }
-amigos.forEach(amigo => {
-    const li = document.createElement("li"); // Cria um elemento <li>
-    li.textContent = amigo; // Define o texto do <li> com o nome do amigo
-    lista.appendChild(li); // Adiciona o <li> na lista <ul>
-});
-function limparCampo{
-    inserirNome = document.querySelector('input');
-    inserirNome.value = "";
-
-}
-function sortearAmigo{
+function sortearAmigo(){
     if (amigos.length<0){
         alert("Por favor, insira um nome");
     }
     let gerador = Math.floor(Math.random() * amigos.length);
     let amigoSorteado = amigos[gerador];
-    document.getElementById("resultado").innerHTML = `Amigo sorteado: ${amigoSorteado}`;
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `Amigo sorteado: ${amigoSorteado}`;
 }
